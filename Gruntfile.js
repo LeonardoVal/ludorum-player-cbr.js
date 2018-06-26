50d9fa01991521aedc0cca0d2a5fc04e52566e4d
Leonardo Val <leonardo.val@creatartis.com>
2018-06-26 20:02:09 -0300

    Setup build with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+﻿/** Gruntfile for [ludorum-player-cbr.js](http://github.com/LeonardoVal/ludorum-player-cbr.js).
+*/
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	});
+
+	require('creatartis-grunt').config(grunt, {
+		sourceNames: ['__prologue__', 'CBRDatabase', 'CBRPlayer', '__epilogue__'],
+		deps: [
+			{ id: 'creatartis-base', name: 'base' },
+			{ id: 'sermat', name: 'Sermat',
+				path: 'node_modules/sermat/build/sermat-umd.js' },
+			{ id: 'ludorum' }
+		],
+		targets: {
+			build_umd: {
+				fileName: 'build/ludorum-player-cbr',
+				wrapper: 'umd'
+			},
+			build_raw: {
+				fileName: 'build/ludorum-player-cbr-tag',
+				wrapper: 'tag'
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['build']);
+};
