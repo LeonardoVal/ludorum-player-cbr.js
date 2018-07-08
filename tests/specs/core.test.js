445f4e1291b95e2239d17ffd8776b6e8c9979ca5
Leonardo Val <leonardo.val@creatartis.com>
2018-07-08 16:52:18 -0300

    More test cases using TicTacToe.

--- tests/specs/core.test.js
+++ tests/specs/core.test.js
@@ -13,29 +13,53 @@ define(['creatartis-base', 'ludorum', 'ludorum-player-cbr'], function (base, lud
 	describe("Case bases with TicTacToe", function () { ///////////////////////////////////////////
 		/*
 		*/
-		function encodingTicTacToe(game, moves) {
-			return {
-				features: game.board.split('').map(function (chr) {
-					return chr === 'X' ? 0 : chr === 'O' ? 1 : 0.5; 
-				}),
-				actions: !moves ? null : game.players.map(function (p) {
-					return moves.hasOwnProperty(p) ? moves[p] : null;
-				})
-			};
-		}
-
-		var game = new ludorum.games.TicTacToe(),
-			memCB = new ludorumCBR.dbs.MemoryCaseBase({ 
-				game: game, 
-				encoding: encodingTicTacToe 
-			});
+		var game = new ludorum.games.TicTacToe();
 
 		it("encoding", function () {
-			var _case0 = memCB.encoding(game);
+			var _case0 = ludorumCBR.utils.encodings.TicTacToe(game);
 			expect(Array.isArray(_case0.features)).toBe(true);
 			expect(_case0.features.join('|')).toBe('0.5|0.5|0.5|0.5|0.5|0.5|0.5|0.5|0.5');
 		});
 
+		it("populate MemoryCaseBase", function (done) {
+			var memCB = new ludorumCBR.dbs.MemoryCaseBase({ 
+				game: game, 
+				encoding: ludorumCBR.utils.encodings.TicTacToe 
+			});
+			expect(memCB.cases().toArray().length).toBe(0);
+			memCB.populate({ n: 1 }).then(function () {
+				expect(memCB.cases().toArray().length).toBeGreaterThan(0);
+				done();
+			});
+		});
+
+		it("playing CBRPlayer with empty case base", function (done) {
+			var memCB = new ludorumCBR.dbs.MemoryCaseBase({ 
+					game: game, 
+					encoding: ludorumCBR.utils.encodings.TicTacToe 
+				}),
+				cbrPlayer = new ludorumCBR.CBRPlayer({ caseBase: memCB, k: 10 }),
+				match = new ludorum.Match(game, [cbrPlayer, cbrPlayer]);
+			match.run().then(function () {
+				expect(match.history.length).toBeGreaterThan(4);
+				done();
+			});
+		});
+
+		it("playing CBRPlayer with populated case base", function (done) {
+			var memCB = new ludorumCBR.dbs.MemoryCaseBase({ 
+					game: game, 
+					encoding: ludorumCBR.utils.encodings.TicTacToe 
+				});
+			memCB.populate({ n: 10 }).then(function () {
+				var cbrPlayer = new ludorumCBR.CBRPlayer({ caseBase: memCB, k: 5 }),
+					match = new ludorum.Match(game, [cbrPlayer, cbrPlayer]);
+				match.run().then(function () {
+					expect(match.history.length).toBeGreaterThan(4);
+					done();
+				});
+			});
+		});
 	}); // Case bases with TicTacToe.
 
 }); // define.
