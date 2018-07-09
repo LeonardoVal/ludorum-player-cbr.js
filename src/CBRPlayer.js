0a2469e9e7d7b9f1264eb3252c7ff3d81d3b3b81
Leonardo Val <leonardo.val@creatartis.com>
2018-07-09 11:00:53 -0300

    Reworked CBRPlayers's `decision` + Negative actions were considered positively. + Actions evaluated with zero are considered with the negatively evaluated actions.

--- src/CBRPlayer.js
+++ src/CBRPlayer.js
@@ -2,46 +2,61 @@
 
 */
 exports.CBRPlayer = base.declare(ludorum.Player, {
+	/** 
+	*/
 	constructor: function CBRPlayer(params) {
 		ludorum.Player.call(this, params);
 		this.caseBase = params && params.caseBase;
 		this.k = params && params.k || 20;
 	},
 
+	/** A `CBRPlayer` takes the action evaluations from the case base, and splits them into actions
+	with possitive evaluations and the ones with evaluations less than or equal to zero. If there
+	are possitively evaluated actions, one of these is chosen randomly with a probability 
+	proportional to the evaluation. If all actions have non possitive evaluations, one of these is
+	chosen with a probability inversely proportional to the evaluation.   
+	*/
 	decision: function decision(game, role) {
-		var actions = this.caseBase.actionEvaluations(game, role, { k: this.k }),
-			positiveActions = actions.filter(function (t) {
+		var actions = iterable(this.movesFor(game, role)).map(function (action) {
+				return [action +'', [action, 0]];
+			}).toObject();
+		this.caseBase.actionEvaluations(game, role, { k: this.k }).forEach(function (t) {
+			var entry = actions[t[0] +''];
+			if (entry) {
+				entry[1] += t[1];
+			}
+		});
+		var minEval = +Infinity,
+			positiveActions = Object.values(actions).filter(function (t) {
+				minEval = Math.min(minEval, t[1]);
 				return t[1] > 0;
 			}),
-			negativeActions = actions.filter(function (t) {
-				return t[1] < 0;
+			negativeActions = Object.values(actions).filter(function (t) {
+				return t[1] <= 0;
 			}).map(function (t) {
-				return [t[0], -t[1]];
-			});
-		if (positiveActions.length < 1) {
-			if (negativeActions.length < 1) {
-				return this.random.choice(this.movesFor(game, role));
-			} else if (negativeActions.length === 1) {
-				return negativeActions[0][0];
-			} else {
-				return this.random.weightedChoice(this.random.normalizeWeights(negativeActions));
-			}
+				return [t[0], t[1] - minEval];
+			}),
+			result;
+		if (positiveActions.length > 1) {
+			result = this.random.weightedChoice(this.random.normalizeWeights(positiveActions));
 		} else if (positiveActions.length === 1) {
-			return positiveActions[0][0];
+			result = positiveActions[0][0];
 		} else {
-			return this.random.weightedChoice(this.random.normalizeWeights(positiveActions));
+			result = this.random.weightedChoice(this.random.normalizeWeights(negativeActions));
 		}
+		return result;
 	},
 
 	// Utilities. /////////////////////////////////////////////////////////////////////////////////
 
-	assess: function assess(game, player, n) {
+	assess: function assess(player, options) {
 		var cbrPlayer = this,
+			game = this.caseBase.game,
 			evaluation = base.iterable(game.players).map(function (p) {
 				return [p, [0,0,0]];
 			}).toObject(),
-			players = base.Iterable.repeat(player, game.players.length).toArray();
-		n = +n || 100;
+			players = base.Iterable.repeat(player, game.players.length).toArray(),
+			n = options && +options.n || 150;
 		return base.Future.sequence(base.Iterable.range(n), function (i) {
 			var matchPlayers = players.slice(),
 				playerIndex = i % game.players.length,
@@ -49,7 +64,9 @@ exports.CBRPlayer = base.declare(ludorum.Player, {
 			matchPlayers[playerIndex] = cbrPlayer;
 			var match = new ludorum.Match(game, matchPlayers);
 			return match.run().then(function () {
-				//console.log("Match #"+ i +":\n\t"+ match.history.map((g) => g.board).join("\n\t"));//FIXME
+				if (options.logger && i > 0 && i % 10 === 0) {
+					options.logger.info("Assessment ran "+ i +" matches.");
+				}
 				var r = match.result()[playerRole];
 				evaluation[playerRole][r > 0 ? 0 : r < 0 ? 2 : 1]++;
 			});
