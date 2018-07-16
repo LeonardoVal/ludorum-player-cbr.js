1cf10cf8a3f30d7b9306e2d5425cace1495704d7
Leonardo Val <leonardo.val@creatartis.com>
2018-07-16 13:55:23 -0300

    Adapted to changes in `CaseBase.populate`

--- tests/connect4-cbr-test.js
+++ tests/connect4-cbr-test.js
@@ -15,26 +15,29 @@ function encoding_connect4(game, moves, ply) {
 	};
 }
 
-function assessCBR(game, trainer, name) {
-	LOGGER.info("Assessing "+ game.name +" with "+ name +".");
+function assessCBR(game, players, opponents) {
+	if (!Array.isArray(players)) {
+		players = [players];
+	}
+	LOGGER.info("Populating case base for "+ game.name +" with: "+ players.map(function (trainer) {
+		return trainer.name;
+	}).join(', ') +".");
 	var CDB = new ludorumCBR.dbs.SQLiteCaseBase({ 
 		game: game,
 		encoding: encoding_connect4,
-		tableName: 'CB_'+ game.name +'_'+ name 
+		dbpath: 'dbs/'+ game.name.toLowerCase() +'-cbr.sqlite',
+		tableName: 'CB_'+ game.name //+'_'+ trainer.name 
 	});
 	return CDB.populate({ 
-		n: 250,
-		players: [trainer], 
+		n: 2500,
+		trainer: new ludorumCBR.CBRPlayer({ name: 'CBRPlayer', caseBase: CDB, k: 30 }),
+		players: players, 
 		logger: LOGGER 
 	}).then(function () {
-		LOGGER.info("Evaluating CBRPlayer for "+ game.name +" trained with "+ name +".");
-		var player = new ludorumCBR.CBRPlayer({
-				caseBase: CDB, 
-				k: 30
-			});
-		return player.assess(new ludorum.players.RandomPlayer(), { n: 600, logger: LOGGER })
-			.then(function (evaluation) {
-				LOGGER.info("Against RANDOM: "+ JSON.stringify(evaluation));
+		LOGGER.info("Evaluating CBRPlayer for "+ game.name +".");
+		var player = new ludorumCBR.CBRPlayer({ caseBase: CDB, k: 30 });
+		return player.assess(opponents, { n: 600, logger: LOGGER }).then(function (evaluation) {
+				LOGGER.info("Assessment: "+ JSON.stringify(evaluation));
 			});
 	});
 }
@@ -42,12 +45,17 @@ function assessCBR(game, trainer, name) {
 // Main ////////////////////////////////////////////////////////////////////////////////////////////
 
 var GAME = new connect4.ConnectFour(),
-	LOGGER = base.Logger.ROOT;
+	LOGGER = base.Logger.ROOT,
+	RANDOM = new ludorum.players.RandomPlayer({ name: 'RANDOM' }),
+	MCTS10 = new ludorum.players.MonteCarloPlayer({ name: 'MCTS10', simulationCount: 10 }),
+	MCTS50 = new ludorum.players.MonteCarloPlayer({ name: 'MCTS50', simulationCount: 50 }),
+	UCT10 = new ludorum.players.UCTPlayer({ name: 'UCT10', simulationCount: 10 }),
+	UCT50 = new ludorum.players.UCTPlayer({ name: 'UCT50', simulationCount: 50 });
 LOGGER.appendToConsole();
 LOGGER.appendToFile(base.Text.formatDate(new Date(), '"logs/connect4-cbr-test-"yyyymmdd-hhnnss".log"'));
-assessCBR(GAME, new ludorum.players.RandomPlayer(), 'RANDOM')
-.then(assessCBR.bind(null, GAME, new ludorum.players.MonteCarloPlayer({ simulationCount: 10 }), 'MCTS10'))
-.then(assessCBR.bind(null, GAME, new ludorum.players.MonteCarloPlayer({ simulationCount: 50 }), 'MCTS50'))
+assessCBR(GAME, MCTS50, [RANDOM, MCTS10, MCTS50])
+//.then(assessCBR.bind(null, GAME, MCTS10, [RANDOM, MCTS10, MCTS50]))
+//.then(assessCBR.bind(null, GAME, MCTS50, [RANDOM, MCTS10, MCTS50]))
 //.then(assessCBR.bind(null, GAME, new ludorum.players.UCTPlayer({ simulationCount: 10 }), 'UCT10'))
 //.then(assessCBR.bind(null, GAME, new ludorum.players.UCTPlayer({ simulationCount: 50 }), 'UCT50'))
 .then(process.exit);
