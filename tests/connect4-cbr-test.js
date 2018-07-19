41a53e0503797346c79072186f9ab42dcd01e059
Leonardo Val <leonardo.val@creatartis.com>
2018-07-19 12:51:52 -0300

    Case base with the CBR player. + Using the `CBRPlayer` to stuff the case base. + Refactored the code, removed `assessCBR`.

--- tests/connect4-cbr-test.js
+++ tests/connect4-cbr-test.js
@@ -1,45 +1,25 @@
+/** # Connect 4 CBR test
+
+CBR based player test. Populates a case base (SQLite3 database) and assesses the resulting player
+against other players. 
+*/
 var ludorumCBR = require('../build/ludorum-player-cbr'),
 	connect4 = require('@creatartis/ludorum-game-connect4'),
 	ludorum = require('ludorum'),
 	base = require('creatartis-base');
 
+// Connect 4 encoding //////////////////////////////////////////////////////////////////////////////
+
 function encoding_connect4(game, moves, ply) {
 	return {
 		ply: ply,
 		features: game.board.string.split('').map(function (sq) {
-			return sq === '.' ? 0 : +sq + 1;
+			return sq === '.' ? 0 : sq === '0' ? -1 : +1;
 		}),
 		actions: !moves ? null : game.players.map(function (p) {
 			return moves.hasOwnProperty(p) ? moves[p] : null;
 		})
 	};
-}
-
-function assessCBR(game, players, opponents) {
-	if (!Array.isArray(players)) {
-		players = [players];
-	}
-	LOGGER.info("Populating case base for "+ game.name +" with: "+ players.map(function (trainer) {
-		return trainer.name;
-	}).join(', ') +".");
-	var CDB = new ludorumCBR.dbs.SQLiteCaseBase({ 
-		game: game,
-		encoding: encoding_connect4,
-		dbpath: 'dbs/'+ game.name.toLowerCase() +'-cbr.sqlite',
-		tableName: 'CB_'+ game.name //+'_'+ trainer.name 
-	});
-	return CDB.populate({ 
-		n: 2500,
-		trainer: new ludorumCBR.CBRPlayer({ name: 'CBRPlayer', caseBase: CDB, k: 30 }),
-		players: players, 
-		logger: LOGGER 
-	}).then(function () {
-		LOGGER.info("Evaluating CBRPlayer for "+ game.name +".");
-		var player = new ludorumCBR.CBRPlayer({ caseBase: CDB, k: 30 });
-		return player.assess(opponents, { n: 600, logger: LOGGER }).then(function (evaluation) {
-				LOGGER.info("Assessment: "+ JSON.stringify(evaluation));
-			});
-	});
 }
 
 // Main ////////////////////////////////////////////////////////////////////////////////////////////
@@ -50,12 +30,34 @@ var GAME = new connect4.ConnectFour(),
 	MCTS10 = new ludorum.players.MonteCarloPlayer({ name: 'MCTS10', simulationCount: 10 }),
 	MCTS50 = new ludorum.players.MonteCarloPlayer({ name: 'MCTS50', simulationCount: 50 }),
 	UCT10 = new ludorum.players.UCTPlayer({ name: 'UCT10', simulationCount: 10 }),
-	UCT50 = new ludorum.players.UCTPlayer({ name: 'UCT50', simulationCount: 50 });
+	UCT50 = new ludorum.players.UCTPlayer({ name: 'UCT50', simulationCount: 50 }),
+	
+	CDB = new ludorumCBR.dbs.SQLiteCaseBase({ 
+		game: GAME,
+		encoding: encoding_connect4,
+		db: 'dbs/'+ GAME.name.toLowerCase() +'-cbr.sqlite',
+		tableName: 'CB_'+ GAME.name
+	}),
+	CBR30 = new ludorumCBR.CBRPlayer({ name: 'CBR30', caseBase: CDB, k: 30 });
 LOGGER.appendToConsole();
 LOGGER.appendToFile(base.Text.formatDate(new Date(), '"logs/connect4-cbr-test-"yyyymmdd-hhnnss".log"'));
-assessCBR(GAME, MCTS50, [RANDOM, MCTS10, MCTS50])
-//.then(assessCBR.bind(null, GAME, MCTS10, [RANDOM, MCTS10, MCTS50]))
-//.then(assessCBR.bind(null, GAME, MCTS50, [RANDOM, MCTS10, MCTS50]))
-//.then(assessCBR.bind(null, GAME, new ludorum.players.UCTPlayer({ simulationCount: 10 }), 'UCT10'))
-//.then(assessCBR.bind(null, GAME, new ludorum.players.UCTPlayer({ simulationCount: 50 }), 'UCT50'))
-.then(process.exit);
+
+// Case base stuffing //////////////////////////////////////////////////////////////////////////////
+
+var PLAYERS = [RANDOM, CBR30],
+	OPPONENTS = [RANDOM, MCTS10, MCTS50];
+
+LOGGER.info("Populating case base for "+ GAME.name +" with: "+ PLAYERS.map(function (p) {
+	return p.name;
+}).join(', ') +".");
+return CDB.populate({ 
+	n: 500,
+	trainer: CBR30,
+	players: PLAYERS,
+	logger: LOGGER 
+}).then(function () {
+	LOGGER.info("Evaluating CBRPlayer for "+ GAME.name +".");
+	return CBR30.assess(OPPONENTS, { n: 600, logger: LOGGER }).then(function (evaluation) {
+			LOGGER.info("Assessment: "+ JSON.stringify(evaluation));
+		});
+}).then(process.exit);
