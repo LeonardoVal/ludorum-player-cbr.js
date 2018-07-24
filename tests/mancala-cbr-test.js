784d7b32e9b0a2d1a3f8cf941ed4afc0e056a6de
LeonardoVal <leonardo.val@creatartis.com>
2018-07-23 22:09:21 -0300

    Updated Mancala test

--- tests/mancala-cbr-test.js
+++ tests/mancala-cbr-test.js
@@ -1,7 +1,14 @@
+/** # Mancala CBR test
+
+CBR based player test. Populates a case base (SQLite3 database) and assesses the resulting player
+against other players. 
+*/
 var ludorumCBR = require('../build/ludorum-player-cbr'),
 	mancala = require('@creatartis/ludorum-game-mancala'),
 	ludorum = require('ludorum'),
 	base = require('creatartis-base');
+
+// Game encoding ///////////////////////////////////////////////////////////////////////////////////
 
 function encoding_mancala(game, moves, ply) {
 	return {
@@ -13,39 +20,52 @@ function encoding_mancala(game, moves, ply) {
 	};
 }
 
-function assessCBR(game, trainer, name) {
-	LOGGER.info("Assessing "+ game.name +" with "+ name +".");
-	var CDB = new ludorumCBR.dbs.SQLiteCaseBase({ 
-		game: game,
-		encoding: encoding_mancala,
-		tableName: 'CB_'+ game.name +'_'+ name 
-	});
-	return CDB.populate({ 
-		n: 250,
-		players: [trainer], 
-		logger: LOGGER 
-	}).then(function () {
-		LOGGER.info("Evaluating CBRPlayer for "+ game.name +" trained with "+ name +".");
-		var player = new ludorumCBR.CBRPlayer({
-				caseBase: CDB, 
-				k: 30
-			});
-		return player.assess(new ludorum.players.RandomPlayer(), { n: 300, logger: LOGGER })
-			.then(function (evaluation) {
-				LOGGER.info("Against RANDOM: "+ JSON.stringify(evaluation));
-			});
-	});
-}
-
 // Main ////////////////////////////////////////////////////////////////////////////////////////////
 
 var GAME = new mancala.Mancala(),
-	LOGGER = base.Logger.ROOT;
+	LOGGER = base.Logger.ROOT,
+	RANDOM = new ludorum.players.RandomPlayer({ name: 'RANDOM' }),
+	MCTS10 = new ludorum.players.MonteCarloPlayer({ name: 'MCTS10', simulationCount: 10 }),
+	MCTS50 = new ludorum.players.MonteCarloPlayer({ name: 'MCTS50', simulationCount: 50 }),
+	UCT10 = new ludorum.players.UCTPlayer({ name: 'UCT10', simulationCount: 10 }),
+	UCT50 = new ludorum.players.UCTPlayer({ name: 'UCT50', simulationCount: 50 }),
+	
+	CDB = new ludorumCBR.dbs.SQLiteCaseBase({ 
+		game: GAME,
+		encoding: encoding_mancala,
+		db: 'dbs/'+ GAME.name.toLowerCase() +'-cbr.sqlite',
+		tableName: 'CB_'+ GAME.name
+	}),
+	CBR10 = new ludorumCBR.CBRPlayer({ name: 'CBR10', caseBase: CDB, k: 10 }),
+	CBR20 = new ludorumCBR.CBRPlayer({ name: 'CBR20', caseBase: CDB, k: 20 }),
+	CBR30 = new ludorumCBR.CBRPlayer({ name: 'CBR30', caseBase: CDB, k: 30 });
 LOGGER.appendToConsole();
 LOGGER.appendToFile(base.Text.formatDate(new Date(), '"logs/mancala-cbr-test-"yyyymmdd-hhnnss".log"'));
-assessCBR(GAME, new ludorum.players.RandomPlayer(), 'RANDOM')
-.then(assessCBR.bind(null, GAME, new ludorum.players.MonteCarloPlayer({ simulationCount: 10 }), 'MCTS10'))
-.then(assessCBR.bind(null, GAME, new ludorum.players.MonteCarloPlayer({ simulationCount: 50 }), 'MCTS50'))
-//.then(assessCBR.bind(null, GAME, new ludorum.players.UCTPlayer({ simulationCount: 10 }), 'UCT10'))
-//.then(assessCBR.bind(null, GAME, new ludorum.players.UCTPlayer({ simulationCount: 50 }), 'UCT50'))
-.then(process.exit);
+
+// Case base stuffing //////////////////////////////////////////////////////////////////////////////
+
+var PLAYERS = [RANDOM, CBR10, CBR20, CBR30],
+	OPPONENTS = [RANDOM, MCTS10, MCTS50];
+
+LOGGER.info("Populating case base for "+ GAME.name +" with: "+ PLAYERS.map(function (p) {
+	return p.name;
+}).join(', ') +".");
+return CDB.populate({ 
+	n: 250,
+	trainer: CBR30,
+	players: PLAYERS,
+	logger: LOGGER 
+}).then(function () {
+	LOGGER.info("Evaluating CBRPlayer for "+ GAME.name +".");
+	return CBR10.assess(OPPONENTS, { n: 60, logger: LOGGER }).then(function (evaluation) {
+		LOGGER.info("Assessment CBR10: "+ JSON.stringify(evaluation));
+	}).then(function () {
+		return CBR20.assess(OPPONENTS, { n: 60, logger: LOGGER }).then(function (evaluation) {
+			LOGGER.info("Assessment CBR20: "+ JSON.stringify(evaluation));
+		});
+	}).then(function () {
+		return CBR30.assess(OPPONENTS, { n: 60, logger: LOGGER }).then(function (evaluation) {
+			LOGGER.info("Assessment CBR30: "+ JSON.stringify(evaluation));
+		});
+	});
+}).then(process.exit);
