6027c0ae333e3dbdee83d36f9a7b6704c139af25
LeonardoVal <leonardo.val@creatartis.com>
2018-10-23 19:17:36 -0300

    Test cases for `EquivalenceCBPlayer`

--- tests/specs/tictactoe.test.js
+++ tests/specs/tictactoe.test.js
@@ -3,7 +3,18 @@
 define(['creatartis-base', 'ludorum', 'ludorum-player-cbr'], function (base, ludorum, ludorumCBR) {
 
 	describe("TicTacToe's implementations", function () { /////////////////////////////////
-		
+		function testGameEncoding(cases, expectedCases) {
+			expect(Array.isArray(cases)).toBe(true);
+			expect(cases.length).toBe(expectedCases.length);
+			var expectedStrs = new Set(expectedCases.map(function (expectedCase) {
+				return expectedCase.join('|');	
+			}));
+			cases.forEach(function (_case) {
+				expect(Array.isArray(_case.features)).toBe(true);
+				expect(expectedStrs.has(_case.features.join('|'))).toBe(true);	
+			});
+		}
+
 		it("DirectCBPlayer features", function () {
 			var cbrPlayer = new ludorumCBR.games.TicTacToe.DirectCBPlayer(),
 				game = cbrPlayer.game;
@@ -26,25 +37,28 @@ define(['creatartis-base', 'ludorum', 'ludorum-player-cbr'], function (base, lud
 		it("DirectCase casesFromGame", function () {
 			var cbrPlayer = new ludorumCBR.games.TicTacToe.DirectCBPlayer(),
 				game = cbrPlayer.game;
-			
-			function testGameEncoding(game, features) {
-				var cases = cbrPlayer.casesFromGame(game);
-				expect(Array.isArray(cases)).toBe(true);
-				expect(cases.length).toBe(1);
-				var _case = cases[0];
-				expect(Array.isArray(_case.features)).toBe(true);
-				expect(_case.features.join('|')).toBe(features.join('|'));
-			}
 
-			testGameEncoding(game, [0,0,0,0,0,0,0,0,0]);
+			testGameEncoding(cbrPlayer.casesFromGame(game), [[0,0,0,0,0,0,0,0,0]]);
 			var game1 = game.next({ Xs: 4 });
-			testGameEncoding(game1, [0,0,0,0,1,0,0,0,0]);
+			testGameEncoding(cbrPlayer.casesFromGame(game1), [[0,0,0,0,1,0,0,0,0]]);
 			var game2 = game1.next({ Os: 0 });
-			testGameEncoding(game2, [-1,0,0,0,1,0,0,0,0]);
+			testGameEncoding(cbrPlayer.casesFromGame(game2), [[-1,0,0,0,1,0,0,0,0]]);
 		});
 
-		//TODO equivalent
-		//TODO EquivalenceCase encoding
+		it("EquivalenceCBPlayer casesFromGame", function () {
+			var cbrPlayer = new ludorumCBR.games.TicTacToe.EquivalenceCBPlayer(),
+				game = cbrPlayer.game;
+
+			testGameEncoding(cbrPlayer.casesFromGame(game), [[0,0,0,0,0,0,0,0,0]]);
+			var game1 = game.next({ Xs: 4 });
+			testGameEncoding(cbrPlayer.casesFromGame(game1), [[0,0,0,0,1,0,0,0,0]]);
+			var game2 = game1.next({ Os: 0 });
+			testGameEncoding(cbrPlayer.casesFromGame(game2), [
+				[-1,0,0,0,1,0,0,0,0], [0,0,-1,0,1,0,0,0,0],
+				[0,0,0,0,1,0,-1,0,0], [0,0,0,0,1,0,0,0,-1]
+			]);
+		});
+
 	}); // TicTacToe's.
 
 }); // define.
