2b625b6e123d26632c0c462cb5a2515eb7747337
LeonardoVal <leonardo.val@creatartis.com>
2018-10-22 22:01:28 -0300

    Updated `TicTacToe` case based implementations + Direct is done. + Equivalent is WIP.

--- src/games/tictactoe.js
+++ src/games/tictactoe.js
@@ -1,14 +1,37 @@
-/**
+/** # TicTacToe CBR
  
 */
 games.TicTacToe = (function () {
-	function directFeatures(game) {
+	/** ## Features direct from the board ####################################################### */
+	
+	var directFeatures = function features(game) {
 		var board = typeof game === 'string' ? game : game.board;
 		return board.split('').map(function (chr) {
 			return chr === 'X' ? (+1) : chr === 'O' ? (-1) : 0; 
 		});
-	}
+	};
 
+	var DirectCBPlayer = declare(CaseBasedPlayer, {
+		constructor: function DirectCBPlayer(params) {
+			CaseBasedPlayer.call(this, params);
+		}, 
+
+		game: new ludorum.games.TicTacToe(),
+
+		features: directFeatures,
+		
+		casesFromGame: function casesFromGame(game, ply, moves) {
+			return [
+				this.newCase(game, ply, moves, this.features(game))
+			];
+		}
+	}); // declare TicTacToe.DirectCBPlayer
+
+	/** ## Equivalence based on board symmetries and rotations. ################################# */
+
+	/** `MAPPINGS` is a list of square indexes that define transformations between equivalent
+	Tictactoe boards.
+	*/
 	var MAPPINGS = [
 		[0,1,2,3,4,5,6,7,8], // original
 		[2,1,0,5,4,3,8,7,6], // vertical axis symmetry
@@ -20,7 +43,7 @@ games.TicTacToe = (function () {
 		[0,3,6,1,4,7,2,5,8]  // 90 clockwise rotation + vertical axis symmetry
 	];
 
-	function equivalent(game) {
+	var equivalent = function equivalent(game) {
 		var board = typeof game === 'string' ? game : game.board,
 			maps = MAPPINGS.map(function (mapping) {
 				return mapping.map(function (i) {
@@ -29,53 +52,47 @@ games.TicTacToe = (function () {
 			});
 		maps.sort();
 		return maps;
-	}
+	};
 
-	return {
-		directFeatures: directFeatures,
+	var EquivalenceCBPlayer = declare(CaseBasedPlayer, {
+		constructor: function EquivalenceCBPlayer(params) {
+			CaseBasedPlayer.call(this, params);
+		}, 
 
-		/**
+		game: new ludorum.games.TicTacToe(),
+
+		features: directFeatures,
+
+		MAPPINGS: MAPPINGS,
+
+		/** 
 		*/
-		DirectCase: declare(Case, {
-			'static fromGame': function fromGame(game, ply, moves) {
-				var _case = new this({
-						ply: +ply,
-						features: directFeatures(game),
-						actions: Case.actionsFromMoves(game.players, moves),
-						results: Case.emptyResults(game.players)
-					});
-				return [_case];
+		casesFromGame: function fromGame(game, ply, moves) {
+			var cbrPlayer = this,
+				board = game.board.split(''),
+				activePlayer = game.activePlayer();
+			if (moves) {
+				board[moves[activePlayer]] = '!';
 			}
-		}),
-
-		equivalent: equivalent,
-
-		/**
-		*/
-		EquivalenceCase: declare(Case, {
-			'static fromGame': function fromGame(game, ply, moves) {
-				var board = game.board.split(''),
-					activePlayer = game.activePlayer();
-				if (moves) {
-					board[moves[activePlayer]] = '!';
-				}
-				var boards = equivalent(board.join('')).map(function (b) {
-					var m = b.indexOf('!');
-					return b.replace('!', '_') + m;
-				});
-				boards.sort();
-				board = boards[0];
+			var boards = equivalent(board.join('')).map(function (b) {
+				var m = b.indexOf('!');
+				return b.replace('!', '_') + m;
+			});
+			return boards.map(function  (board) {
 				if (moves) {
 					moves[activePlayer] = +(board.substr(9));
 				}
-				var _case = new this({
-						ply: +ply,
-						features: directFeatures(board.substr(0,9)),
-						actions: Case.actionsFromMoves(game.players, moves),
-						results: Case.emptyResults(game.players)
-					});
-				return [_case];
-			}
-		})
+				return cbrPlayer.newCase(game, ply, moves, cbrPlayer.features(board.substr(0,9)));
+			});
+		}
+	}); // declare TicTacToe.EquivalenceCBPlayer
+
+	return {
+		directFeatures: directFeatures,
+		DirectCBPlayer: DirectCBPlayer,
+
+		MAPPINGS: MAPPINGS,
+		equivalent: equivalent,
+		EquivalenceCBPlayer: EquivalenceCBPlayer
 	};
-})(); // declare TicTacToe.DirectCase
+})();
