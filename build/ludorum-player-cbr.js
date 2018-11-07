ed9f91555a26aacc1b3e44c03769704bc64037c0
Leonardo Val <leonardo.val@creatartis.com>
2018-11-07 13:34:33 -0300

    New build

--- build/ludorum-player-cbr.js
+++ build/ludorum-player-cbr.js
@@ -28,10 +28,12 @@ function __init__(base, Sermat, ludorum) { "use strict";
 			__SERMAT__: { include: [base, ludorum] },
 
 			dbs: { /* Namespace for different types of case bases. */ },
-			games: { /* Namespace for functions and definitions for supporting games. */ }
+			games: { /* Namespace for functions and definitions for supporting games. */ },
+			utils: { /* Namespace for utility functions. */ }
 		},
 		dbs = exports.dbs,
-		games = exports.games
+		games = exports.games,
+		utils = exports.utils
 	;
 
 
@@ -452,48 +454,6 @@ var CaseBasedPlayer = exports.CaseBasedPlayer = base.declare(ludorum.Player, {
 			result = this.random.weightedChoice(this.random.normalizeWeights(negativeActions));
 		}
 		return result;
-	},
-
-	// Utilities. /////////////////////////////////////////////////////////////////////////////////
-
-	assess: function assess(players, options) { //FIXME
-		if (!Array.isArray(players)) {
-			players = [players];
-		}
-		var cbrPlayer = this,
-			game = this.game,
-			evaluation = iterable(players).map(function (player) {
-				return [player.name, iterable(game.players).map(function (p) {
-						return [p, [0,0,0]];
-					}).toObject()];
-				}).toObject(),
-			n = options && +options.n || 300,
-			finishedMatchesCount = 0,
-			intervalId = 0;
-		if (options.logger) {
-			intervalId = setInterval(function () {
-				options.logger.info("Assessment finished "+ finishedMatchesCount +" matches.");
-			}, options.logTime || 30000);
-		}
-		return base.Future.sequence(base.Iterable.range(n).product(players), function (tuple) {
-			var player = tuple[1],
-				matchPlayers = base.Iterable.repeat(player, game.players.length).toArray(),
-				playerIndex = tuple[0] % game.players.length,
-				playerRole = game.players[playerIndex];
-			matchPlayers[playerIndex] = cbrPlayer;
-			var match = new ludorum.Match(game, matchPlayers);
-			return match.run().then(function () {
-				var r = match.result()[playerRole];
-				evaluation[player.name][playerRole][r > 0 ? 0 : r < 0 ? 2 : 1]++;
-				finishedMatchesCount++;
-			});
-		}).then(function () {
-			clearInterval(intervalId);
-			if (options.logger) {
-				options.logger.info("Assessment finished "+ finishedMatchesCount +" matches.");
-			}
-			return evaluation;
-		});
 	}
 }); // declare CBRPlayer
 
@@ -777,6 +737,70 @@ games.TicTacToe = (function () {
 /** # Utilities
 
 */
+
+utils.assess = function assess(cbPlayer, options) {
+	var opponents = options.opponents || 
+			options.opponent ? [options.opponent] : new ludorum.players.RandomPlayer(),
+		game = options.game || cbPlayer.game,
+		evaluation = iterable(opponents).map(function (player) {
+			return [player.name, iterable(game.players).map(function (p) {
+					return [p, [0,0,0]];
+				}).toObject()];
+			}).toObject(),
+		assessCount = options && +options.assessCount || 300,
+		finishedMatchesCount = 0,
+		intervalId = 0,
+		logger = options.logger;
+	if (logger) {
+		intervalId = setInterval(function () {
+			logger.info("Assessment finished "+ finishedMatchesCount +" matches.");
+		}, options.logTime || 30000);
+	}
+	return base.Future.sequence(base.Iterable.range(assessCount).product(opponents), function (tuple) {
+		var player = tuple[1],
+			matchPlayers = base.Iterable.repeat(player, game.players.length).toArray(),
+			playerIndex = tuple[0] % game.players.length,
+			playerRole = game.players[playerIndex];
+		matchPlayers[playerIndex] = cbPlayer;
+		var match = new ludorum.Match(game, matchPlayers);
+		return match.run().then(function () {
+			var r = match.result()[playerRole];
+			evaluation[player.name][playerRole][r > 0 ? 0 : r < 0 ? 2 : 1]++;
+			finishedMatchesCount++;
+		});
+	}).then(function () {
+		clearInterval(intervalId);
+		if (logger) {
+			logger.info("Assessment finished "+ finishedMatchesCount +" matches.");
+		}
+		return evaluation;
+	});
+};
+
+utils.populateAndAssess = function populateAndAssess(player, options) {
+	var name = options.name || player.name,
+		logger = options.logger,
+		game = options.game || player.game;
+	if (logger) {
+		logger.info("Assessing "+ game.name +" with "+ name +".");
+	}
+	return player.populate({ 
+		n: options.populateCount || 1000,
+		trainer: options.trainer || new ludorum.players.RandomPlayer(),
+		logger: logger 
+	}).then(function () {
+		logger.info("Evaluating player for "+ game.name +" trained with "+ name +".");
+		return base.Future.sequence(options.opponents || [new ludorum.players.RandomPlayer()], function (opponent) {
+			return utils.assess(player, {
+					opponent: opponent,
+					assessCount: options.assessCount || 800, 
+					logger: logger 
+				}).then(function (evaluation) {
+					logger.info("Against "+ opponent.name +": "+ JSON.stringify(evaluation));
+				});
+		});
+	});
+};
 
 
 // See __prologue__.js
