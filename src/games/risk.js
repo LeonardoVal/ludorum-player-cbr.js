54675cd458d0c7d0a886f6a64b737a575db3e393
mgonnet <gonnet.matias@gmail.com>
2018-12-20 02:11:30 -0300

    add risky game to games.Risk.DirectCBPlayer

--- src/games/risk.js
+++ src/games/risk.js
@@ -5,10 +5,23 @@ games.Risk = (function () {
   var DirectCBPlayer = declare(CaseBasedPlayer, {
     constructor: function DirectCBPlayer(params){
       CaseBasedPlayer.call(this, params)
-    }
+    },
+
+    game: new ludorum_risky.Risk({
+      boardMap: ludorum_risky.maps.test01,
+      armies: {
+        WhiteCountry:  ['White', 6],
+        YellowCountry: ['Yellow', 6],
+        RedCountry:    ['Red', 6],
+        GreenCountry:  ['Green', 6],
+        BlueCountry:   ['Blue', 6],
+        BlackCountry:  ['Black', 6],
+      }
+     }),
+
   })
 
   return {
-    
+    DirectCBPlayer
   };
 })();
