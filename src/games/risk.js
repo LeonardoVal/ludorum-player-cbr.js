715d6971a1c1661c7f40e2e115e713b9bd068b24
mgonnet <gonnet.matias@gmail.com>
2018-12-20 01:19:24 -0300

    Add constructor to games.Risk.DirectCBPlayer

--- src/games/risk.js
+++ src/games/risk.js
@@ -2,6 +2,12 @@
  */
 games.Risk = (function () {  
   
+  var DirectCBPlayer = declare(CaseBasedPlayer, {
+    constructor: function DirectCBPlayer(params){
+      CaseBasedPlayer.call(this, params)
+    }
+  })
+
   return {
     
   };
